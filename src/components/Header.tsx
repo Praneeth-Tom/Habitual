e25a9e654a0c39afeb4109df3af99d4241d2a4f9
Praneeth Tom <praneeth.tom@gmail.com>
2025-06-22 22:15:14 +0000

    add a light and dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { CreateHabitDialog } from "@/components/CreateHabitDialog";
+import { ModeToggle } from "./ModeToggle";
 
 type HeaderProps = {
   addHabit: (name: string) => void;
@@ -11,8 +12,9 @@ export default function Header({ addHabit }: HeaderProps) {
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6 text-primary"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"></path><path d="m9 12 2 2 4-4"></path></svg>
         <h1 className="text-xl font-bold text-primary">Habitual</h1>
       </div>
-      <div className="ml-auto">
+      <div className="ml-auto flex items-center gap-2">
         <CreateHabitDialog addHabit={addHabit} />
+        <ModeToggle />
       </div>
     </header>
   );
